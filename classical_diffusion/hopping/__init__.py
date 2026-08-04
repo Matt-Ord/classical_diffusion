e3285a9f459c6b42a84cf36585b24183a18aa55e
Matthew Ord <matthew.ord1@gmail.com>
2026-08-04 11:23:47 +0000

    create analysis file

--- classical_diffusion/hopping/__init__.py
+++ classical_diffusion/hopping/__init__.py
@@ -1,11 +1,20 @@
 """Simulations using a hopping model."""
 
-from ._hopping import HoppingSimulationResult, solve_ensemble
+from ._analysis import plot_deterministic_isf
+from ._hopping import (
+    DeterministicSolverResult,
+    HoppingSimulationResult,
+    get_ensemble_probabilities,
+    solve_ensemble,
+)
 from ._system import Lattice, Lattice1D
 
 __all__ = [
+    "DeterministicSolverResult",
     "HoppingSimulationResult",
     "Lattice",
     "Lattice1D",
+    "get_ensemble_probabilities",
+    "plot_deterministic_isf",
     "solve_ensemble",
 ]
