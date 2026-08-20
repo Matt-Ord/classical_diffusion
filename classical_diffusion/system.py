Fix units etc

classical_diffusion/system.py
@@ -65,6 +65,23 @@ class UnitSystem:
     ) -> float | np.ndarray[Any, np.dtype[np.number]]:
         """Convert time from self units into target units (defaults to SI)."""
         units = units or UnitSystem.si()
+        return value * (units.time_factor / self.time_factor)
+
+    @overload
+    def frequency_into(
+        self, value: float, units: UnitSystem | None = None
+    ) -> float: ...
+    @overload
+    def frequency_into[DT: np.dtype[np.number]](
+        self, value: np.ndarray[Any, DT], units: UnitSystem | None = None
+    ) -> np.ndarray[Any, DT]: ...
+    def frequency_into(
+        self,
+        value: float | np.ndarray[Any, np.dtype[np.number]],
+        units: UnitSystem | None = None,
+    ) -> float | np.ndarray[Any, np.dtype[np.number]]:
+        """Convert frequency from self units into target units (defaults to SI)."""
+        units = units or UnitSystem.si()
         return value * (self.time_factor / units.time_factor)
 
     @overload
@@ -80,7 +97,7 @@ class UnitSystem:
     ) -> float | np.ndarray[Any, np.dtype[np.number]]:
         """Convert mass from self units into target units (defaults to SI)."""
         units = units or UnitSystem.si()
-        return value * (self.mass_factor / units.mass_factor)
+        return value * (units.mass_factor / self.mass_factor)
 
     @overload
     def length_into(self, value: float, units: UnitSystem | None = None) -> float: ...
@@ -95,7 +112,7 @@ class UnitSystem:
     ) -> float | np.ndarray[Any, np.dtype[np.number]]:
         """Convert length from self units into target units (defaults to SI)."""
         units = units or UnitSystem.si()
-        return value * (self.length_factor / units.length_factor)
+        return value * (units.length_factor / self.length_factor)
 
     @overload
     def momentum_into(self, value: float, units: UnitSystem | None = None) -> float: ...
@@ -110,7 +127,7 @@ class UnitSystem:
     ) -> float | np.ndarray[Any, np.dtype[np.number]]:
         """Convert momentum from self units into target units (defaults to SI)."""
         units = units or UnitSystem.si()
-        momentum_factor = self.momentum_factor / units.momentum_factor
+        momentum_factor = units.momentum_factor / self.momentum_factor
         return value * momentum_factor
 
     @overload
@@ -126,7 +143,7 @@ class UnitSystem:
     ) -> float | np.ndarray[Any, np.dtype[np.number]]:
         """Convert energy from self units into target units (defaults to SI)."""
         units = units or UnitSystem.si()
-        return value * (self.energy_factor / units.energy_factor)
+        return value * (units.energy_factor / self.energy_factor)
 
     def as_canonical(self) -> CanonicalUnitSystem:
         """Get a jax-compatible unit system."""

examples/ballistic_langevin/full_isf_plots/2d_fcc.py
@@ -21,15 +21,13 @@ system = PeriodicSystemFCC(
     barrier_energy=4e-21,
 )
 
-normalized_system = system.with_normalized_units()
-
 
 def _plot_periodic_isf() -> None:
 
     full_result = solve_ensemble(
-        normalized_system,
+        system,
         TimeSpan(
-            t_end=normalized_system.units.time_into(10e-12),
+            t_end=system.units.time_into(10e-12),
             n_steps=1000,
         ),
         (np.full((500, 2), 1.0), np.full((500, 2), 0.0)),
@@ -47,9 +45,9 @@ def _plot_periodic_isf() -> None:
     line_0.set_label("full simulation")
 
     ballistic_result = solve_ballistic_ensemble(
-        normalized_system,
+        system,
         TimeSpan(
-            t_end=normalized_system.units.time_into(10e-12),
+            t_end=system.units.time_into(10e-12),
             n_steps=5000,
         ),
         n_samples=100000,
@@ -63,7 +61,7 @@ def _plot_periodic_isf() -> None:
     elastic_result, inelastic_result = breakdown_ballistic_trajectory(
         ballistic_result,
         minimum_timescale=get_diffusion_time(
-            normalized_system, characteristic_length=normalized_system.delta_x
+            system, characteristic_length=system.delta_x
         ),
     )
 
@@ -79,15 +77,9 @@ def _plot_periodic_isf() -> None:
     line_3.set_label("inelastic")
     line_3.set_linestyle(":")
 
-    ax.set_xlim(
-        0,
-        2e-12,
-    )
+    ax.set_xlim(0, 2e-12)
 
-    ax.set_ylim(
-        0.0,
-        1.0,
-    )
+    ax.set_ylim(0.0, 1.0)
 
     ax.legend(handles=[line_0, line_1, line_2, line_3])
     fig.savefig(

examples/ballistic_langevin/potentials_and_trajectories/1d_periodic.py
@@ -21,8 +21,6 @@ system = PeriodicSystem1D(
     barrier_energy=10e-21,
 )
 
-normalized_system = system.with_normalized_units()
-
 
 def _plot_periodic_system() -> None:
 
@@ -36,9 +34,9 @@ def _plot_periodic_system() -> None:
 def _plot_ballistic_trajectory() -> None:
 
     result = solve_single_ballistic(
-        normalized_system,
+        system,
         TimeSpan(
-            t_end=normalized_system.units.time_into(10e-12),
+            t_end=system.units.time_into(10e-12),
             n_steps=1000,
         ),
         (np.full((1,), 0.0), np.full((1,), 2.43)),
@@ -47,7 +45,7 @@ def _plot_ballistic_trajectory() -> None:
     elastic, inelastic = breakdown_ballistic_trajectory(
         result,
         minimum_timescale=get_diffusion_time(
-            normalized_system, characteristic_length=normalized_system.delta_x / 0.1
+            system, characteristic_length=system.delta_x / 0.1
         ),
     )
 

examples/ballistic_langevin/potentials_and_trajectories/2d_fcc.py
@@ -19,8 +19,6 @@ system = PeriodicSystemFCC(
     barrier_energy=1.6e-21,
 )
 
-normalized_system = system.with_normalized_units()
-
 
 def _plot_periodic_system() -> None:
 
@@ -34,9 +32,9 @@ def _plot_periodic_system() -> None:
 def _plot_ballistic_trajectory() -> None:
 
     result = solve_single_ballistic(
-        normalized_system,
+        system,
         TimeSpan(
-            t_end=normalized_system.units.time_into(50e-12),
+            t_end=system.units.time_into(50e-12),
             n_steps=1000,
         ),
         (np.full((2,), 1), np.full((2,), 0.01)),
@@ -44,9 +42,7 @@ def _plot_ballistic_trajectory() -> None:
 
     elastic, inelastic = breakdown_ballistic_trajectory(
         result,
-        minimum_timescale=get_diffusion_time(
-            normalized_system, 1 / normalized_system.gamma
-        ),
+        minimum_timescale=get_diffusion_time(system, 1 / system.gamma),
     )
 
     fig, ax = get_two_panel_figure()

examples/distribution.py
@@ -41,14 +41,12 @@ def _plot_xp_distributions_periodic() -> None:
         barrier_energy=1.6e-21,
     )
 
-    normalized_system = system.with_normalized_units()
-
     fig, ax = get_fancy_figure()
 
     result = solve_ensemble(
-        normalized_system,
+        system,
         TimeSpan(
-            t_end=normalized_system.units.time_into(1000 / system.gamma),
+            t_end=system.units.time_into(1000 / system.gamma),
             n_steps=1000,
         ),
         (np.full((10, 1), 0), np.full((10, 1), 0.0)),
@@ -65,7 +63,7 @@ def _plot_xp_distributions_periodic() -> None:
     ax.set_ylim(-1e23, 1e-23)
     fig.savefig("examples/distribution.1d_periodic.p.pdf")
 
-    result_folded = fold_results(result, delta=normalized_system.delta_x)
+    result_folded = fold_results(result, delta=system.delta_x)
 
     fig, ax = get_fancy_figure()
     _, ax, _bars = plot_x_histogram(result=result_folded.with_si_units(), ax=ax)
@@ -128,14 +126,12 @@ def _plot_x_distribution_spread() -> None:
 def _plot_xp_distributions_harmonic() -> None:
     system = HarmonicSystem(gamma=5e11, temperature=110, m=8e-27, omega=10e12)
 
-    normalized_system = system.with_normalized_units()
-
     fig, ax = get_fancy_figure()
 
     result = solve_ensemble(
-        normalized_system,
+        system,
         TimeSpan(
-            t_end=normalized_system.units.time_into(2000 / system.gamma),
+            t_end=system.units.time_into(2000 / system.gamma),
             n_steps=2000,
         ),
         (np.full((10, 1), 0), np.full((10, 1), 0.0)),