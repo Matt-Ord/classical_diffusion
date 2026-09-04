23621f65790a21b231ba153e62a62c1f67712c12
jd_br1 <jdb211@cam.ac.uk>
2026-09-04 10:31:10 +0000

    Rate form trajectories

--- classical_diffusion/jax/langevin/__init__.py
+++ classical_diffusion/jax/langevin/__init__.py
@@ -1,16 +1,25 @@
 """Jax-native Simulations of a Physical System."""
 
+from classical_diffusion.jax.langevin._analysis import (
+    filter_trajectory,
+    get_trajectory_breakpoints,
+)
 from classical_diffusion.jax.langevin._langevin import (
     solve_many,
     solve_many_overdamped,
 )
 
-from ._system import KramersParametersJax as KramersParameters
-from ._system import KramersSystem1D
+from classical_diffusion.jax.langevin._system import (
+    KramersParametersJax as KramersParameters,
+)
+from classical_diffusion.jax.langevin._system import KramersSystem1D
 
 __all__ = [
     "KramersParameters",
     "KramersSystem1D",
+    "filter_trajectory",
+    "get_isf_offset",
+    "get_trajectory_breakpoints",
     "solve_many",
     "solve_many_overdamped",
 ]
